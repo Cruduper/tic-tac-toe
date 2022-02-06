b4b59e319cfed30fe822019db49b2fd708d68052
Eric Crudup <briefcasemanx@gmail.com>
2022-02-05 19:06:47 -0800

    mostly playable

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,20 @@ function Game() {
   this.board = new Board();
   this.player1 = new Player("x");
   this.player2 = new Player("o");
+  this.gameOver = false;
 }
 
 
 Game.prototype.gameTurn = function( placement ) {
-  if ( (this.player1.turn === true) && ( this.board.boardArr[placement[0]][placement[1]] === "") )  {
+
+  const row = placement[0];
+  const col = placement[1];
+  const boardPlace = this.board.boardArr[row][col];
+
+  if ( (this.player1.turn === true) && ( boardPlace === "") )  {
     this.board.addToBoard( this.player1.symbol, placement);
   }
-  else if ( (this.player2.turn === true) && (this.board.boardArr[placement[0]][placement[1]] === "") )  {
+  else if ( (this.player2.turn === true) && ( boardPlace === "") )  {
     this.board.addToBoard( this.player2.symbol, placement);
   }
   else  {
@@ -23,10 +29,12 @@ Game.prototype.gameTurn = function( placement ) {
 
   if ( this.calcWinner(this.player1.symbol) ) {
     alert("player 1 wins!");
+    this.gameOver = true;
     this.newGame();
   }
   else if ( this.calcWinner(this.player2.symbol) )  {
     alert("player 2 wins!")
+    this.gameOver = true;
     this.newGame();
   }
 
@@ -109,8 +117,89 @@ function Player(symb) {
   this.myTurn; 
 }
 
+
+//UI Logic
+
+
+let game = new Game();
+game.newGame();
+
+
+function oneTurn (thisArg) {
+
+  const row = $(thisArg).closest("tr").index();
+  const col = $(thisArg).index();
+
+  if ( $(thisArg).text() === "") {
+    if (game.player1.turn == true)  {
+      $(thisArg).text(game.player1.symbol);
+      $("#nameOne").toggleClass("turnRed");
+      $("#nameTwo").toggleClass("turnRed");
+    }
+    else  {  
+      $(thisArg).text(game.player2.symbol);
+      $("#nameOne").toggleClass("turnRed");
+      $("#nameTwo").toggleClass("turnRed");
+    }
+
+    game.gameTurn([ row, col ]);
+
+    if (game.gameOver == true)  {
+      $(".squares").text("");
+      $("#nameOne").addClass("turnRed");
+      $("#nameTwo").removeClass("turnRed");
+      game.gameOver = false;
+    }
+  }
+  else  {
+    alert("That square is already occupied! Click somewhere else!");
+  }
+}
+
+function bothNamesFilled(name1, name2)  {
+  if ( ( name1 != "") && ( name2 != "") ) {
+    $(".playerBox").hide();
+    $(".grid").show();
+    $(".names").show();
+  }
+}
+
+
 $(document).ready(function() {
+  let name1 = "";
+  let name2 = "";
+
+  $("button#p1Button").click( function() {
+    name1 = $("input#p1Name").val();
+    bothNamesFilled(name1, name2);
+    $("#nameOne").text(name1);
+  });
 
 
+  $("button#p2Button").click( function() {
+    name2 = $("input#p2Name").val();
+    bothNamesFilled(name1, name2);
+     $("#nameTwo").text(name2);
+  });
+
+  // if ( ($("playerOne").text() != "") && ($("playerOne").text() != "") ) {
+  //   $("playerBox").hide();
+  //   $("#grid").show();
+  // }
+
+  $(".squares").click( function() {
+    
+    oneTurn(this);
+    
+
+
+      
+
+
+  });
+
+
+  //var colIndex = $(this).closest("td").index();
+  //var rowIndex = $(this).closest("tr").index();
 
 });
